9abdf4f1dd71e1686c8e02f57ee3b4753c06329b
José Segura <jose.segura.ms@gmail.com>
2026-02-01 11:13:10 -0300

    update: styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { LandingPage } from '@pages/index';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './styles/components.css'; // Classes utilitárias
-import './styles/style.css'; // Este já importa o variables.css
+import './styles/components.css';
+import './styles/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
